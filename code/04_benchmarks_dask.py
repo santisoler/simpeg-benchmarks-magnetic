bf9a236cb898d3fb417d70b770b02b9a3900ade7
Santiago Soler <santisoler@fastmail.com>
2024-05-24 16:36:46 -0700

    Update dask script

--- code/04_benchmarks_dask.py
+++ code/04_benchmarks_dask.py
@@ -1,13 +1,13 @@
 """
-Benchmark mag simulation with Dask
+Benchmark mag simulation with Dask.
 
-Run simulation in parallel.
+Run only with geoana as engine, since the runs with choclo will be the same as
+in the parallel script.
 """
 
 from pathlib import Path
 import itertools
 import numpy as np
-import xarray as xr
 import simpeg.dask  # noqa: F401
 from simpeg import maps
 
@@ -24,8 +24,12 @@ from utilities import (
 
 # Define some variables common to all benchmarks
 # ----------------------------------------------
-height = 100  # height of the observation points
+# These parameters should be the same as in the parallel script.
 mesh_spacings = (10, 10, 5)
+n_cells_per_axis = [20, 40, 60, 80, 100]
+
+grid_shape = (60, 60)  # shape of the receivers grid
+height = 100  # height of the observation points
 
 # Create results dir if it doesn't exists
 results_dir = Path(__file__).parent / ".." / "results"
@@ -35,21 +39,14 @@ if not results_dir.exists():
 
 # Create iterator
 # ---------------
-# Use only geoana as engine (but now with dask)
-engine = "geoana"
-
-# Read iterator values from the dataset resulting from running the
-# 02_benchmarks_parallel.py script.
-fname = results_dir / "benchmarks_parallel.nc"
-ds = xr.load_dataset(fname)
-forward_only_values = ds.forward_only.values.tolist()
-n_receivers_values = ds.n_receivers.values.tolist()
-n_cells_values = ds.n_cells.values.tolist()
+forward_only_values = [True, False]
+n_cells_values = [n**3 for n in n_cells_per_axis]
+fields = ["tmi", "b"]
 
 iterators = (
     forward_only_values,
-    n_receivers_values,
     n_cells_values,
+    fields,
 )
 pool = itertools.product(*iterators)
 
@@ -58,30 +55,33 @@ pool = itertools.product(*iterators)
 # ----------
 n_runs = 3
 
-dims = ("forward_only", "n_receivers", "n_cells")
+dims = ("forward_only", "n_cells", "field")
 coords = {
     "forward_only": forward_only_values,
-    "n_receivers": n_receivers_values,
     "n_cells": n_cells_values,
+    "field": fields,
 }
 data_names = ["times", "times_std"]
 results = create_dataset(dims, coords, data_names)
+results.attrs = dict(n_receivers=np.prod(grid_shape))
 
-for index, (forward_only, n_receivers, n_cells) in enumerate(pool):
+for index, (
+    forward_only,
+    n_cells,
+    field,
+) in enumerate(pool):
     if index > 0:
         print()
     print("Running benchmark")
     print("-----------------")
     print(
         f"  forward_only: {forward_only} \n"
-        f"  n_receivers: {n_receivers} \n"
-        f"  n_cells: {n_cells}"
+        f"  n_cells: {n_cells} \n"
+        f"  field: {field} \n"
     )
 
-    grid_shape = tuple(int(np.sqrt(n_receivers)) for _ in range(2))
-    mesh_shape = tuple(int(n_cells ** (1 / 3)) for _ in range(3))
-
     # Define mesh
+    mesh_shape = tuple(int(n_cells ** (1 / 3)) for _ in range(3))
     mesh, active_cells = create_tensor_mesh(mesh_shape, mesh_spacings)
     n_active_cells = np.sum(active_cells)
     susceptibility = create_susceptibilty(n_active_cells)
@@ -89,7 +89,10 @@ for index, (forward_only, n_receivers, n_cells) in enumerate(pool):
 
     # Define receivers and survey
     grid_coords = create_observation_points(get_region(mesh), grid_shape, height)
-    survey = create_survey(grid_coords)
+    components = field
+    if components == "b":
+        components = ["bx", "by", "bz"]
+    survey = create_survey(grid_coords, components=components)
 
     # Define benchmarker
     store_sensitivities = "forward_only" if forward_only else "ram"
@@ -98,9 +101,8 @@ for index, (forward_only, n_receivers, n_cells) in enumerate(pool):
         mesh=mesh,
         ind_active=active_cells,
         chiMap=model_map,
-        engine=engine,
+        engine="geoana",
         store_sensitivities=store_sensitivities,
-        n_processes=None,
     )
     benchmarker = SimulationBenchmarker(n_runs=n_runs, **kwargs)
 
@@ -108,9 +110,13 @@ for index, (forward_only, n_receivers, n_cells) in enumerate(pool):
     runtime, std = benchmarker.benchmark(susceptibility)
 
     # Save results
-    indices = dict(forward_only=forward_only, n_receivers=n_receivers, n_cells=n_cells)
+    indices = dict(
+        forward_only=forward_only,
+        n_cells=n_cells,
+        field=field,
+    )
     results.times.loc[indices] = runtime
     results.times_std.loc[indices] = std
 
     # Write results to file
-    results.to_netcdf(results_dir / "benchmarks_dask.nc")
+    results.to_netcdf(results_dir / "benchmarks-dask.nc")
