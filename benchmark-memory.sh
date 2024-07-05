6a2ee768aa577861af5020125723661eeaf2ecf5
Santiago Soler <santisoler@fastmail.com>
2024-07-05 14:39:33 -0700

    Fix benchmark-memory.sh script

--- benchmark-memory.sh
+++ benchmark-memory.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-command time -v python notebooks/large_model.py choclo 2> results/memory-choclo
-command time -v python notebooks/large_model.py geoana 2> results/memory-geoana
-command time -v python notebooks/large_model.py dask 2> results/memory-dask
+command time -v python code/large_model.py choclo 2> results/memory-choclo
+command time -v python code/large_model.py geoana 2> results/memory-geoana
+command time -v python code/large_model.py dask 2> results/memory-dask
